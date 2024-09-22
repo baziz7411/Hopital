13f01557cdc5eada8860f4c423af2d46ed2f1fcb
BAZIZ Abdelhalim <baziz.halim94@gmail.com>
2024-09-22 11:26:03 +0200

    Patients Controller Services

--- src/main/java/com/hospital/patients/PatientsApplication.java
+++ src/main/java/com/hospital/patients/PatientsApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class PatientsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PatientsApplication.class, args);
 	}
 
---
+++ src/main/java/com/hospital/patients/controller/PatientsController.java
@@ -0,0 +1,22 @@
+package com.hospital.patients.controller;
+
+
+import com.hospital.patients.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientsController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("/name_patient")
+    public String printPtientsName(){
+        return patientService.printName("baziz halim");
+    }
+
+}
